05ae6f9d4a33bb8d425c545eb92957f02e19ea73
Armando Capriles <armandotechno12@gmail.com>
2023-05-22 19:29:49 -0400

    Agregando un estilo para el botón de navegar

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
-import { Button, Text, View } from 'react-native';
+import { Button, Text, TouchableOpacity, View } from 'react-native';
 import { Logo } from '../components/Logo';
 import { StackScreenProps } from '@react-navigation/stack';
+import { buttonStyle } from '../theme/buttonTheme';
 
 interface Props extends StackScreenProps<any, any>{};
 
@@ -11,11 +12,17 @@ export const HomeScreen = ( { navigation }: Props ) => {
         // justifyContent: 'center'
         flex: 1
       }}>
-        <View>
-          <Button 
-            title='Ver Comida'
+        <View style={{
+          // backgroundColor: 'red'
+          padding: 10
+        }}>
+          <TouchableOpacity
+            activeOpacity={ 0.7 }
             onPress={ () => navigation.navigate('ComidaScreen') }
-          />
+          >
+            <Text style={ buttonStyle.text }>Comida</Text>
+          </TouchableOpacity>
+          
         </View>
 
         <View style={{
---
+++ src/theme/buttonTheme.tsx
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const buttonStyle = StyleSheet.create({
+    text: {
+        color: 'black',
+        fontSize: 18
+    }
+});
